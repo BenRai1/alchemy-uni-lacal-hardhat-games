ae29647599ea0cf0302ded5a3c07942d799ffb17
BenRai1 <benrainow@gmx.de>
2022-12-26 14:58:58 +0100

    Games solved

---
+++ scripts/win5.js
@@ -0,0 +1,29 @@
+// add the game address here and update the contract name if necessary
+const gameAddr = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788";
+const contractName = "Game5";
+
+async function main() {
+  // attach to the game
+  const game = await hre.ethers.getContractAt(contractName, gameAddr);
+
+  //increas the balance of the allowance
+  await game.giveMeAllowance(10000);
+  // mint the value of the allowance to the balance
+  await game.mint(10000);
+
+  // do whatever you need to do to win the game here:
+
+  const tx = await game.win();
+
+  // did you win? Check the transaction receipt!
+  // if you did, it will be in both the logs and events array
+  const receipt = await tx.wait();
+  console.log(receipt);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
